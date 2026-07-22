fa149a845b983fddd8daeb97398f3b54725e5535
ops523 <ops@adwallz.com>
2026-07-22 16:41:24 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+from init_db import initialize_database
+
+initialize_database()
+
+st.set_page_config(
+
+    page_title="AIMS ERP",
+
+    layout="wide"
+
+)
+
+st.title("AIMS ERP")
+
+st.success("Foundation Loaded Successfully")
+
+st.write("Sprint 1 Version 2")
