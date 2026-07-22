adf01fb0c53a3861329b7a8dd7a2cda73d07bb1a
ops523 <ops@adwallz.com>
2026-07-22 16:59:17 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,13 @@
-"""
-Import every ORM model here.
-"""
-
 from models.user import User
+from models.supplier import Supplier
+from models.manufacturer import Manufacturer
+from models.media_product import MediaProduct
+from models.warehouse import Warehouse
 
 __all__ = [
     "User",
+    "Supplier",
+    "Manufacturer",
+    "MediaProduct",
+    "Warehouse",
 ]
