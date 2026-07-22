dd3f92b60e9e5d5a456f4cd19f82b4d5c28116e6
ops523 <ops@adwallz.com>
2026-07-22 17:02:43 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,11 +3,23 @@ from models.supplier import Supplier
 from models.manufacturer import Manufacturer
 from models.media_product import MediaProduct
 from models.warehouse import Warehouse
+from models.media_roll import MediaRoll
+from models.inventory_transaction import InventoryTransaction
 
 __all__ = [
+
     "User",
+
     "Supplier",
+
     "Manufacturer",
+
     "MediaProduct",
+
     "Warehouse",
+
+    "MediaRoll",
+
+    "InventoryTransaction"
+
 ]
