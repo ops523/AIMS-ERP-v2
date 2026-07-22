1a1d454556c02c99caf0fad26b242b1c5358a9b5
ops523 <ops@adwallz.com>
2026-07-22 17:05:49 +0530

    Create wastage_master.py

---
+++ utils/wastage_master.py
@@ -0,0 +1,39 @@
+WASTAGE_REASONS = [
+
+"Short Roll",
+
+"Media Defect",
+
+"Printing Head Strike",
+
+"Ink Smudge",
+
+"Colour Issue",
+
+"Bleed Cut",
+
+"Machine Calibration",
+
+"Operator Error",
+
+"Artwork Revision",
+
+"Wrong Artwork",
+
+"Packing Damage",
+
+"Transportation Damage",
+
+"QC Rejected",
+
+"Trial Print",
+
+"Roll End Waste",
+
+"Edge Alignment",
+
+"Media Tear",
+
+"Other"
+
+]
