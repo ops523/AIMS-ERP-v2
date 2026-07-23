c867d14cfa1d3f38e9d5db550deac4ae2690c291
ops523 <ops@adwallz.com>
2026-07-23 19:13:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,6 @@ st.title("AIMS ERP")
 st.success("Foundation Loaded Successfully")
 
 st.write("Sprint 1 Version 2")
+
+if page == "Campaign Import Wizard":
+    from pages.campaign_wizard import *
