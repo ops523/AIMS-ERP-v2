355abddb4b79b34a0c576345a1eb8fd813dab857
ops523 <ops@adwallz.com>
2026-07-24 12:43:10 +0530

    Create activity_service.py

---
+++ services/activity_service.py
@@ -0,0 +1,23 @@
+from repositories.activity_log_repository import (
+    ActivityLogRepository,
+)
+
+
+class ActivityService:
+
+    @staticmethod
+    def log(
+        db,
+        module,
+        reference,
+        activity,
+        user,
+    ):
+
+        ActivityLogRepository.log(
+            db=db,
+            module=module,
+            reference=reference,
+            activity=activity,
+            performed_by=user,
+        )
