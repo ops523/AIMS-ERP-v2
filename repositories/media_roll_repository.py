26520a18ae279ca97fb9ea6a0ae135eb39ce3b05
ops523 <ops@adwallz.com>
2026-07-24 12:46:11 +0530

    Create media_roll_repository.py

---
+++ repositories/media_roll_repository.py
@@ -0,0 +1,55 @@
+from sqlalchemy.orm import Session
+
+from models.media_roll import MediaRoll
+
+
+class MediaRollRepository:
+
+    @staticmethod
+    def create(
+        db: Session,
+        media_roll: MediaRoll,
+    ):
+
+        db.add(media_roll)
+
+        db.commit()
+
+        db.refresh(media_roll)
+
+        return media_roll
+
+    @staticmethod
+    def get_all(
+        db: Session,
+    ):
+
+        return (
+
+            db.query(MediaRoll)
+
+            .order_by(
+                MediaRoll.id.desc()
+            )
+
+            .all()
+
+        )
+
+    @staticmethod
+    def get_by_id(
+        db: Session,
+        roll_id: int,
+    ):
+
+        return (
+
+            db.query(MediaRoll)
+
+            .filter(
+                MediaRoll.id == roll_id
+            )
+
+            .first()
+
+        )
