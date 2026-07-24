83d69f5353279b382f8a8ef42044952012a6e3a3
ops523 <ops@adwallz.com>
2026-07-24 12:52:29 +0530

    Update inventory_transaction_repository.py

--- repositories/inventory_transaction_repository.py
+++ repositories/inventory_transaction_repository.py
@@ -1,13 +1,50 @@
-@staticmethod
-def create(
-    db,
-    transaction,
-):
+from sqlalchemy.orm import Session
 
-    db.add(transaction)
+from models.inventory_transaction import InventoryTransaction
 
-    db.commit()
 
-    db.refresh(transaction)
+class InventoryTransactionRepository:
 
-    return transaction
+    @staticmethod
+    def create(
+        db: Session,
+        transaction: InventoryTransaction,
+    ):
+
+        db.add(transaction)
+
+        db.commit()
+
+        db.refresh(transaction)
+
+        return transaction
+
+    @staticmethod
+    def get_all(
+        db: Session,
+    ):
+
+        return (
+            db.query(InventoryTransaction)
+            .order_by(
+                InventoryTransaction.id.desc()
+            )
+            .all()
+        )
+
+    @staticmethod
+    def get_by_roll(
+        db: Session,
+        media_roll_id: int,
+    ):
+
+        return (
+            db.query(InventoryTransaction)
+            .filter(
+                InventoryTransaction.media_roll_id == media_roll_id
+            )
+            .order_by(
+                InventoryTransaction.id.desc()
+            )
+            .all()
+        )
