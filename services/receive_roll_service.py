1005baf60946a64b80a67f8bf546da25bdc79888
ops523 <ops@adwallz.com>
2026-07-24 13:20:25 +0530

    Update receive_roll_service.py

--- services/receive_roll_service.py
+++ services/receive_roll_service.py
@@ -27,147 +27,140 @@ class ReceiveRollService:
         remarks=None,
     ):
 
-        # --------------------------------------------------
-        # Generate Roll Code
-        # --------------------------------------------------
-
-        roll_code = CodeGenerator.generate(
-            db=db,
-            model=MediaRoll,
-            code_field="roll_code",
-            prefix="ROLL",
-        )
-
-        # --------------------------------------------------
-        # Calculate Nominal Sq Ft
-        # --------------------------------------------------
-
-        nominal_sqft = round(
-            roll_width * nominal_length,
-            2,
-        )
-
-        # --------------------------------------------------
-        # Create Media Roll
-        # --------------------------------------------------
-
-        media_roll = MediaRoll(
-
-            roll_code=roll_code,
-
-            supplier_id=supplier_id,
-
-            manufacturer_id=manufacturer_id,
-
-            media_product_id=media_product_id,
-
-            warehouse_id=warehouse_id,
-
-            width_ft=roll_width,
-
-            length_ft=nominal_length,
-
-            total_sqft=nominal_sqft,
-
-            balance_sqft=nominal_sqft,
-
-        )
-
-        media_roll = MediaRollRepository.create(
-            db,
-            media_roll,
-        )
-
-        # --------------------------------------------------
-        # Save Roll Measurement
-        # --------------------------------------------------
-
-        RollMeasurementService.create_measurement(
-
-            db=db,
-
-            media_roll_id=media_roll.id,
-
-            measured_length_ft=measured_length,
-
-            measured_width_ft=measured_width,
-
-            measured_by=received_by,
-
-            remarks=remarks,
-
-        )
-
-        # --------------------------------------------------
-        # Create Inventory Ledger Entry
-        # --------------------------------------------------
-
-        InventoryTransactionService.post_transaction(
-
-            db=db,
-
-            media_roll_id=media_roll.id,
-
-            transaction_type="RECEIPT",
-
-            reference_module="Receive Roll",
-
-            qty_in=media_roll.total_sqft,
-
-            qty_out=0,
-
-            reference_id=media_roll.id,
-
-            remarks="Media Roll Received",
-
-            user=received_by,
-
-        )
-
-        # --------------------------------------------------
-        # Activity Log
-        # --------------------------------------------------
-
-        ActivityService.log(
-
-            db=db,
-
-            module="Inventory",
-
-            reference=roll_code,
-
-            activity="Media Roll Received",
-
-            user=received_by,
-
-        )
-
-        # --------------------------------------------------
-        # TODO (Sprint 4.2)
-        # --------------------------------------------------
-        # Generate QR Code
-        # Save QR Image
-        # Print QR Sticker
-        # --------------------------------------------------
-
-        return media_roll
-
         try:
 
-        media_roll = MediaRollRepository.create(...)
+            # ----------------------------------------
+            # Generate Roll Code
+            # ----------------------------------------
 
-        RollMeasurementService.create_measurement(...)
+            roll_code = CodeGenerator.generate(
+                db=db,
+                model=MediaRoll,
+                code_field="roll_code",
+                prefix="ROLL",
+            )
 
-        InventoryTransactionService.post_transaction(...)
+            # ----------------------------------------
+            # Calculate Nominal Sq Ft
+            # ----------------------------------------
 
-        ActivityService.log(...)
+            nominal_sqft = round(
+                roll_width * nominal_length,
+                2,
+            )
 
-        db.commit()
+            # ----------------------------------------
+            # Create Media Roll
+            # ----------------------------------------
 
-        return media_roll
+            media_roll = MediaRoll(
+
+                roll_code=roll_code,
+
+                supplier_id=supplier_id,
+
+                manufacturer_id=manufacturer_id,
+
+                media_product_id=media_product_id,
+
+                warehouse_id=warehouse_id,
+
+                width_ft=roll_width,
+
+                length_ft=nominal_length,
+
+                total_sqft=nominal_sqft,
+
+                balance_sqft=nominal_sqft,
+
+            )
+
+            media_roll = MediaRollRepository.create(
+                db=db,
+                media_roll=media_roll,
+            )
+
+            # ----------------------------------------
+            # Save Measurement
+            # ----------------------------------------
+
+            RollMeasurementService.create_measurement(
+
+                db=db,
+
+                media_roll_id=media_roll.id,
+
+                measured_length_ft=measured_length,
+
+                measured_width_ft=measured_width,
+
+                measured_by=received_by,
+
+                remarks=remarks,
+
+            )
+
+            # ----------------------------------------
+            # Inventory Ledger Entry
+            # ----------------------------------------
+
+            InventoryTransactionService.post_transaction(
+
+                db=db,
+
+                media_roll_id=media_roll.id,
+
+                transaction_type="RECEIPT",
+
+                reference_module="Receive Roll",
+
+                qty_in=media_roll.total_sqft,
+
+                qty_out=0,
+
+                reference_id=media_roll.id,
+
+                remarks="Media Roll Received",
+
+                user=received_by,
+
+            )
+
+            # ----------------------------------------
+            # Activity Log
+            # ----------------------------------------
+
+            ActivityService.log(
+
+                db=db,
+
+                module="Inventory",
+
+                reference=roll_code,
+
+                activity="Media Roll Received",
+
+                user=received_by,
+
+            )
+
+            # ----------------------------------------
+            # Future (Sprint 4.2)
+            # QR Generation
+            # ----------------------------------------
+
+            # ----------------------------------------
+            # Commit Complete Transaction
+            # ----------------------------------------
+
+            db.commit()
+
+            db.refresh(media_roll)
+
+            return media_roll
 
         except Exception:
 
-        db.rollback()
+            db.rollback()
 
-        raise
+            raise
