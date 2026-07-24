b6f00062b0cdd6db4602baaac98c6d9e3b71c49f
ops523 <ops@adwallz.com>
2026-07-24 12:42:36 +0530

    Create activity_log_repository.py

---
+++ repositories/activity_log_repository.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+
+from models.activity_log import ActivityLog
+
+
+class ActivityLogRepository:
+
+    @staticmethod
+    def log(
+        db: Session,
+        module: str,
+        reference: str,
+        activity: str,
+        performed_by: str,
+    ):
+
+        obj = ActivityLog(
+            module=module,
+            reference=reference,
+            activity=activity,
+            performed_by=performed_by,
+        )
+
+        db.add(obj)
+        db.commit()
