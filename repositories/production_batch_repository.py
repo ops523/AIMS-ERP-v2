2678b43a1eb7c8df4c158568cb5b7a0fc8d9a966
ops523 <ops@adwallz.com>
2026-07-26 13:07:04 +0530

    Create production_batch_repository.py

---
+++ repositories/production_batch_repository.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Session
+
+from models.production_batch import ProductionBatch
+
+
+class ProductionBatchRepository:
+
+    @staticmethod
+    def create(
+        db: Session,
+        batch: ProductionBatch,
+    ):
+        db.add(batch)
+        db.flush()
+        return batch
+
+    @staticmethod
+    def get_all(
+        db: Session,
+    ):
+        return (
+            db.query(ProductionBatch)
+            .order_by(
+                ProductionBatch.id.desc()
+            )
+            .all()
+        )
+
+    @staticmethod
+    def get(
+        db: Session,
+        batch_id: int,
+    ):
+        return (
+            db.query(ProductionBatch)
+            .filter(
+                ProductionBatch.id == batch_id
+            )
+            .first()
+        )
