0288c1cac2c8087dda59106cc9d99fff4cf58a4c
ops523 <ops@adwallz.com>
2026-07-26 13:19:56 +0530

    Update init.py

--- constants/init.py
+++ constants/init.py
@@ -2,4 +2,6 @@ from .inventory import *
 from .modules import *
 from .roles import *
 from .status import *
-from .production import *
+
+from .production_batch_status import *
+from .production_item_status import *
