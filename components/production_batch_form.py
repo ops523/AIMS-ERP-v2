b78e3abcaccdcc46b86e1f5e1467d58ebb799630
ops523 <ops@adwallz.com>
2026-07-26 15:58:23 +0530

    Update production_batch_form.py

--- components/production_batch_form.py
+++ components/production_batch_form.py
@@ -2,10 +2,6 @@ import streamlit as st
 
 from database import SessionLocal
 
-from repositories.campaign_repository import (
-    CampaignRepository,
-)
-
 from repositories.printer_repository import (
     PrinterRepository,
 )
@@ -22,39 +18,21 @@ class ProductionBatchForm:
 
         db = SessionLocal()
 
-        campaigns = CampaignRepository.get_all(db)
-
         printers = PrinterRepository.get_all(db)
 
-        campaign = st.selectbox(
-
-            "Campaign",
-
-            campaigns,
-
-            format_func=lambda x: x.campaign_name,
-
-        )
-
         printer = st.selectbox(
-
             "Printer",
-
             printers,
-
             format_func=lambda x: x.printer_name,
-
         )
 
         remarks = st.text_area("Remarks")
 
-        if st.button("Create Batch"):
+        if st.button("Create Production Batch"):
 
             batch = ProductionBatchService.create(
 
                 db=db,
-
-                campaign_id=campaign.id,
 
                 printer_id=printer.id,
 
@@ -65,9 +43,7 @@ class ProductionBatchForm:
             db.commit()
 
             st.success(
-
-                f"Production Batch {batch.batch_number} created."
-
+                f"Batch {batch.batch_number} created successfully."
             )
 
             st.rerun()
