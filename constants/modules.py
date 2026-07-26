6df94e79cb48cbb9dd5e816babd403b1c84870d5
ops523 <ops@adwallz.com>
2026-07-26 12:44:41 +0530

    Create modules.py

---
+++ constants/modules.py
@@ -0,0 +1,15 @@
+RECEIVE_ROLL = "Receive Roll"
+
+PRINTING = "Printing"
+
+PACKAGING = "Packaging"
+
+DISPATCH = "Dispatch"
+
+STOCK_ADJUSTMENT = "Stock Adjustment"
+
+WAREHOUSE_TRANSFER = "Warehouse Transfer"
+
+CAMPAIGN_IMPORT = "Campaign Import"
+
+ROLL_MEASUREMENT = "Roll Measurement"
