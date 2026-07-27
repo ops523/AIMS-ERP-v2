4b014807f8cf543e0228c2a9e1ba76b19216908c
ops523 <ops@adwallz.com>
2026-07-27 12:32:15 +0530

    Update production_batch_wizard.py

--- components/production_batch_wizard.py
+++ components/production_batch_wizard.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 from components.wizard.step_campaign import StepCampaign
 from components.wizard.step_version import StepVersion
+from components.wizard.step_artwork import StepArtwork
+from components.wizard.step_printer import StepPrinter
+from components.wizard.step_rolls import StepRolls
+from components.wizard.step_review import StepReview
 
 
 class ProductionBatchWizard:
@@ -9,10 +13,14 @@ class ProductionBatchWizard:
     @staticmethod
     def render():
 
-        st.subheader("Create Production Batch")
-
         if "wizard_step" not in st.session_state:
             st.session_state.wizard_step = 1
+
+        st.progress(st.session_state.wizard_step / 6)
+
+        st.caption(
+            f"Step {st.session_state.wizard_step} of 6"
+        )
 
         if st.session_state.wizard_step == 1:
             StepCampaign.render()
@@ -20,5 +28,14 @@ class ProductionBatchWizard:
         elif st.session_state.wizard_step == 2:
             StepVersion.render()
 
-        else:
-            st.success("More steps coming in next sprint.")
+        elif st.session_state.wizard_step == 3:
+            StepArtwork.render()
+
+        elif st.session_state.wizard_step == 4:
+            StepPrinter.render()
+
+        elif st.session_state.wizard_step == 5:
+            StepRolls.render()
+
+        elif st.session_state.wizard_step == 6:
+            StepReview.render()
