5512192f43df283669601fa0e48bb0ac6c0d325f
ops523 <ops@adwallz.com>
2026-07-27 11:54:13 +0530

    Update production_batch_service.py

--- services/production_batch_service.py
+++ services/production_batch_service.py
@@ -1,30 +1,54 @@
+from database import SessionLocal
+
 from models.production_batch import ProductionBatch
+
 from repositories.production_batch_repository import (
     ProductionBatchRepository,
 )
-from utils.number_generator import NumberGenerator
+
+from constants.production_batch import DRAFT
 
 
 class ProductionBatchService:
 
     @staticmethod
-    def create(
+    def generate_batch_number():
+
+        db = SessionLocal()
+
+        try:
+
+            count = (
+                ProductionBatchRepository.count(db)
+                + 1
+            )
+
+            return f"PB{count:06d}"
+
+        finally:
+
+            db.close()
+
+    @staticmethod
+    def create_batch(
         db,
         printer_id,
-        remarks=None,
+        remarks="",
     ):
 
         batch = ProductionBatch(
 
-            batch_number=NumberGenerator.production_batch_number(db),
+            batch_number=ProductionBatchService.generate_batch_number(),
 
             printer_id=printer_id,
+
+            status=DRAFT,
 
             remarks=remarks,
 
         )
 
-        return ProductionBatchRepository.create(
-            db=db,
-            batch=batch,
+        return ProductionBatchRepository.add(
+            db,
+            batch,
         )
