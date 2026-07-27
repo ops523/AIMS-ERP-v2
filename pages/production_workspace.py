5ab6db8e50bbfde20ee48f66fd0442657f8439e1
ops523 <ops@adwallz.com>
2026-07-27 12:16:05 +0530

    Update production_workspace.py

--- pages/production_workspace.py
+++ pages/production_workspace.py
@@ -1,24 +1,14 @@
 import streamlit as st
 
-from database import SessionLocal
-
-from components.page_header import PageHeader
-
 from components.production_dashboard import ProductionDashboard
-
-from components.production_batch_form import (
-    ProductionBatchForm,
-)
-
-from components.production_batch_list import (
-    ProductionBatchList,
-)
+from components.production_batch_wizard import ProductionBatchWizard
+from components.production_batch_list import ProductionBatchList
 
 ProductionDashboard.render()
 
 st.divider()
 
-ProductionBatchForm.render()
+ProductionBatchWizard.render()
 
 st.divider()
 
