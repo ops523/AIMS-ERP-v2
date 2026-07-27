f6a74c57f808ea819f0d65a5111cd37e47ce17bb
ops523 <ops@adwallz.com>
2026-07-27 13:33:18 +0530

    Create campaign_artwork_repository.py

---
+++ repositories/campaign_artwork_repository.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+
+from models.campaign_artwork import CampaignArtwork
+
+
+class CampaignArtworkRepository:
+
+    @staticmethod
+    def get_by_version(
+        db: Session,
+        version_id: int,
+    ):
+
+        return (
+            db.query(CampaignArtwork)
+            .filter(
+                CampaignArtwork.campaign_version_id == version_id
+            )
+            .order_by(
+                CampaignArtwork.artwork_name
+            )
+            .all()
+        )
