029390605d74aceb5c75adb9d2ff68e78a7eb483
ops523 <ops@adwallz.com>
2026-07-27 11:53:32 +0530

    Update production_batch_repository.py

--- repositories/production_batch_repository.py
+++ repositories/production_batch_repository.py
@@ -6,31 +6,20 @@ from models.production_batch import ProductionBatch
 class ProductionBatchRepository:
 
     @staticmethod
-    def create(
-        db: Session,
-        batch: ProductionBatch,
-    ):
-        db.add(batch)
-        db.flush()
-        return batch
+    def get_all(db: Session):
 
-    @staticmethod
-    def get_all(
-        db: Session,
-    ):
         return (
             db.query(ProductionBatch)
-            .order_by(
-                ProductionBatch.id.desc()
-            )
+            .order_by(ProductionBatch.id.desc())
             .all()
         )
 
     @staticmethod
-    def get(
+    def get_by_id(
         db: Session,
         batch_id: int,
     ):
+
         return (
             db.query(ProductionBatch)
             .filter(
@@ -38,18 +27,36 @@ class ProductionBatchRepository:
             )
             .first()
         )
-    
+
     @staticmethod
-    def open_batches(db):
+    def get_by_batch_number(
+        db: Session,
+        batch_number: str,
+    ):
 
         return (
-
             db.query(ProductionBatch)
-
             .filter(
-            ProductionBatch.status != "COMPLETED"
+                ProductionBatch.batch_number == batch_number
             )
-
-        .all()
-
+            .first()
         )
+
+    @staticmethod
+    def add(
+        db: Session,
+        batch: ProductionBatch,
+    ):
+
+        db.add(batch)
+        db.commit()
+        db.refresh(batch)
+
+        return batch
+
+    @staticmethod
+    def count(db: Session):
+
+        return db.query(
+            ProductionBatch
+        ).count()
