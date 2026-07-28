9b73eed0d5e5ac4452d35d6d229eadf6799ed125
ops523 <ops@adwallz.com>
2026-07-28 14:03:27 +0530

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -9,6 +9,21 @@ from services.seed_service import seed_database
 
 def initialize_database():
 
+    from sqlalchemy import inspect
+    import streamlit as st
+
+    inspector = inspect(engine)
+
+    st.sidebar.write("Tables before create_all()")
+    st.sidebar.write(inspector.get_table_names())
+
+    Base.metadata.create_all(bind=engine)
+
+    inspector = inspect(engine)
+
+    st.sidebar.write("Tables after create_all()")
+    st.sidebar.write(inspector.get_table_names())
+    
     Base.metadata.create_all(bind=engine)
 
     db = SessionLocal()
