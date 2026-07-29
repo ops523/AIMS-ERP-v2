edc7b0fe6effe9edffaaabb6ef93a86143aa71a0
ops523 <ops@adwallz.com>
2026-07-29 14:31:17 +0530

    Update roll_details.py

--- pages/roll_details.py
+++ pages/roll_details.py
@@ -4,7 +4,7 @@ from core.startup import startup
 
 startup()
 
-from database import SessionLocal
+from database import get_session
 
 from services.roll_details_service import (
     RollDetailsService,
