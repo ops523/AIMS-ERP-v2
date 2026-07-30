3cca5614fef9c561ae39b788b6d33e97ebe34e15
ops523 <ops@adwallz.com>
2026-07-30 16:58:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,11 @@ from core.startup import startup
 
 startup()
 
+from core.storage_manager import StorageManager
+import streamlit as st
+
+st.write(StorageManager.BASE_DIR.resolve())
+
 # ---------------------------------------------------------
 # Dashboard
 # ---------------------------------------------------------
