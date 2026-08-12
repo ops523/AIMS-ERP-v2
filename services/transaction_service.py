4bc167ba707abf760501de194b9cfad68bb1bcbd
ops523 <ops@adwallz.com>
2026-08-12 13:50:56 +0530

    Update transaction_service.py

--- services/transaction_service.py
+++ services/transaction_service.py
@@ -1,12 +1,3 @@
-"""
-ERP Transaction Manager
-
-Centralized transaction handling.
-
-All business services should use this service instead of
-calling commit()/rollback() directly.
-"""
-
 from contextlib import contextmanager
 
 from sqlalchemy.orm import Session
